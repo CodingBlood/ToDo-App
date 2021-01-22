b236034f5e4c2b5e25b55901b4a92bc39be7a696
Kartik Agarwal <kartik.agarwal.192.168@gmail.com>
2021-01-22 13:10:37 +0530

    Enrolling Beta version 1.0.1.0, enabling user to add description to thier ToDo Task

--- app/src/main/java/com/example/android/todoapplication/MainActivity.kt
+++ app/src/main/java/com/example/android/todoapplication/MainActivity.kt
@@ -1,20 +1,61 @@
 package com.example.android.todoapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Typeface
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.marginBottom
 import kotlinx.android.synthetic.main.activity_main.*
+
+
+
+
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var result : String = "Your ToDo List is as follows : \n\n"
+        var todo = hashMapOf<Int,String>()
+        var todoDesc = hashMapOf<Int,String>()
+        var myButtons = ArrayList<Button>()
+        var linearlayt = LinearLayout(this@MainActivity)
+//        linearlayt.orientation(1)
         var i = 1
+        scrollView2.addView(linearlayt)
         bt.setOnClickListener {
-            result = result + "\n" + i.toString()+ ")  " +et.text.toString()
+            todo [i]= et.text.toString()
+            todoDesc [i]= etdesc.text.toString()
+            var result : String = "Your ToDo List is as follows :"
             tv.text=result
+            var btnView = Button(this@MainActivity)
+            btnView.text = todo[i]
+            myButtons.add(btnView)
+            linearlayt.addView(btnView)
             et.text.clear()
-            i=i+1
+            i += 1
+        }
+        btclear.setOnClickListener {
+            scrollView2.removeView(linearlayt)
+            linearlayt = LinearLayout(this@MainActivity)
+            scrollView2.addView(linearlayt)
+            et.text.clear()
+            myButtons = ArrayList<Button>()
+            todo = hashMapOf<Int,String>()
+            todoDesc = hashMapOf<Int,String>()
+            i=1
         }
     }
 }
+
+
+
+
+
+
